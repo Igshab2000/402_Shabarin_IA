b72dae56dfb854125f698b00642276bf14370f74
Andrey Popov <andrvpopov@gmail.com>
2021-01-25 16:16:16 +0300

    Delete tasks from DB_Applications course

--- task02/minesweeper/src/Controller.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Igshab2000\minesweeper\Controller;
-    use function Igshab2000\minesweeper\View\showGame;
-    
-    function startGame() {
-        echo "Game started".PHP_EOL;
-        showGame();
-    }
-?>
--- task02/minesweeper/src/View.php
+++
@@ -1,5 +0,0 @@
-<?php namespace Igshab2000\minesweeper\View;
-    function showGame() {
-        echo "Game shown";
-    }
-?>
